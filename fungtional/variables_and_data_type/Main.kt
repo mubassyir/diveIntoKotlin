d25c8756908c3c6b4c76108380f059480a1b3f91
mubassyir <amubassyir@gmail.com>
2021-05-01 20:42:56 +0700

    refactor tipe data

--- fungtional/variables_and_data_type/Main.kt
+++ fungtional/variables_and_data_type/Main.kt
@@ -1,12 +1,16 @@
 package fungtional.variables_and_data_type
 
+fun main(){
+    val main = Main()
+    println(main.tipeDataFloat)
+    println(main.tipeDataFloat)
+    println(main.tipeDataString)
+    main.tipeDataFloat = 3.7
+    println(main.tipeDataFloat)
+}
 class Main {
     //variable dan tipe data
-   fun main(){
        val tipeDataString:String = "asd"
        val tipeDataInteger = 123
        var tipeDataFloat = 0.1 // -> mutable variable
-
-       tipeDataFloat = 4.3
-   }
 }
